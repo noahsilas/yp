0b474e71960f0c744cca627efdff77679ec0035d
Noah Silas <noah@silas.cc>
2014-12-07 19:04:55 -0800

    Get a search query from the caller
    The caller enters up to three digits to select who they want to be
    connected to, and Twilio will POST that information back to us.
    
    For testing purposes, for now we just read the input back to the caller.

--- yp.rb
+++ yp.rb
@@ -8,7 +8,24 @@ post '/twilio/incoming' do
   <<-TWIML
 <?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say voice="man">Hello World</Say>
+  <Gather action="/twilio/search" finishOnKey='#' numDigits='3'>
+    <Say voice="man">Enter the first three letters of a name.</Say>
+  </Gather>
+</Response>
+  TWIML
+end
+
+post '/twilio/search' do
+  response = <<-TWIML
+<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  TWIML
+
+  params['Digits'].split('').each do |digit|
+    response << "  <Say>#{digit}</Say>\n"
+  end
+
+  response << <<-TWIML
 </Response>
   TWIML
 end
