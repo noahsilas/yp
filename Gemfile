3e74eafd784846ab4fc37890594045bdfae42a20
Noah Silas <noah@silas.cc>
2014-12-07 20:33:36 -0800

    Pretty up Twilio response syntax
    The `twilio-ruby` gem [1] gives us a nice Builder::XmlMarkup derived
    syntax for generating TwiML, without writing out ugly XML heredocs.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ ruby '2.0.0'
 
 gem "sinatra"
 gem "google-api-client"
+gem "twilio-ruby"
 
 group :test do
   gem 'rspec'
--- yp.rb
+++ yp.rb
@@ -1,31 +1,22 @@
 require 'sinatra'
+require 'twilio-ruby'
 
 get '/' do
   'YellowPages'
 end
 
 post '/twilio/incoming' do
-  <<-TWIML
-<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Gather action="/twilio/search" finishOnKey='#' numDigits='3'>
-    <Say voice="man">Enter the first three letters of a name.</Say>
-  </Gather>
-</Response>
-  TWIML
+  Twilio::TwiML::Response.new do |r|
+    r.Gather(action: '/twilio/search', finishOnKey: '#', numDigits: 3) do
+      r.Say('Enter the first three letters of a name.', voice: 'woman')
+    end
+  end.text
 end
 
 post '/twilio/search' do
-  response = <<-TWIML
-<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  TWIML
-
-  params['Digits'].split('').each do |digit|
-    response << "  <Say>#{digit}</Say>\n"
-  end
-
-  response << <<-TWIML
-</Response>
-  TWIML
+  Twilio::TwiML::Response.new do |r|
+    params['Digits'].split('').each do |digit|
+      r.Say digit
+    end
+  end.text
 end
